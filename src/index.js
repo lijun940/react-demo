a7dae77db729076c3bb011c58bf7c06e5ce547e7
lijun <junli@hitotek.com>
2023-10-25 20:47:48 +0800

    f

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
-import {Component} from 'react'
-import ReactDOM from 'react-dom'
-import Hello from './components/Hello'
-import Demo from './components/Demo'
+import { Component } from "react";
+import ReactDOM from "react-dom";
+import Hello from "./components/Hello";
+import Demo from "./components/Demo";
 
 class App extends Component {
   state = {
-    msg: 'hello',
-     count: 0
-  }
+    msg: "hello",
+    count: 0,
+  };
   render() {
     return (
       <div>
         <h1>我是跟组件</h1>
         <div>{this.state.msg}</div>
         <div>{this.state.count}</div>
+        <button onClick={this.clickFn}>+1</button>
       </div>
-    )
+    );
+  }
+  clickFn = () => {
+    console.log("点击事件", this);
+  };
+  mouseFn() {
+    console.log("鼠标进入事件");
   }
 }
-ReactDOM.render(<App/>, document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector("#root"));
